dddb6b824bfb001c0b981a9978d97bf613c70f96
vitoorsouzaa1 <vitor.souza407@yahoo.com.br>
2022-12-24 11:17:46 -0300

    fix: add return to CustomButton component

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -1,7 +1,9 @@
 import './CustomButton.scss'
 
 export const CustomButton = ({ onClick, children }) => {
-  ;<div className="custom-button-container" onClick={onClick}>
-    {children}
-  </div>
+  return (
+    <div className="custom-button-container" onClick={onClick}>
+      {children}
+    </div>
+  )
 }
